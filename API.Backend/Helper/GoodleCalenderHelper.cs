326448b6f94308c30e1e4bb58ceddeba05546671
Ramahadi Moheane <moheanerj@gmail.com>
2022-11-15 01:38:11 +0200

    added goggle calender helper

---
+++ API.Backend/Helper/GoodleCalenderHelper.cs
@@ -0,0 +1,52 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Util.Store;
+
+namespace API.Backend.Helper
+{
+    public class GoodleCalenderHelper
+    {
+        public static async Task<Event> CreateGoogleCalendar(GoogleCalendar request)
+        {
+            string[] Scopes = { "https://www.googleapis.com/auth/calendar" };
+            string ApplicationName = "Google Canlendar Api";
+            UserCredential credential;
+            using (var stream = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "Cre", "cre.json"), FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+            // define services
+            var services = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            // define request
+            Event eventCalendar = new Event()
+            {
+                Summary = request.Summary,
+                Location = request.Location,
+                Start = new EventDateTime
+                {
+                    DateTime = request.Start,
+                    TimeZone = "Asia/Ho_Chi_Minh"
+                },
+                End = new EventDateTime
+                {
+                    DateTime = request.End,
+                    TimeZone = "Asia/Ho_Chi_Minh"
+                },
+                Description = request.Description
+            };
+            var eventRequest = services.Events.Insert(eventCalendar, "primary");
+            var requestCreate = await eventRequest.ExecuteAsync();
+            return requestCreate;
+        }
+    }
+}
---
+++ API.Backend/Models/GoogleCalender.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Backend.Models
+{
+    public class GoogleCalender
+    {
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
